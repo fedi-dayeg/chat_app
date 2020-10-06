b5d51ce30c8ea26fac321c2b42b5cfee8e8ae6dc
fedi <fedi.dayeg@hotmail.com>
2020-10-06 20:12:58 +0100

    first commit

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,66 @@
+import 'package:chat_app/widgets/auth/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  var _isLoading = false;
+
+  void _submitAuthForm(String email, String password, String username,
+      bool isLogin, BuildContext ctx) async {
+    AuthResult authResult;
+
+    try {
+      if (isLogin) {
+        setState(() {
+          _isLoading = true;
+        });
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({'username': username, 'email': email});
+      }
+    } on PlatformException catch (error) {
+      var message = 'An error occured, please check you credentials!';
+      if (error.message != null) {
+        message = error.message;
+      }
+
+      Scaffold.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (error) {
+      print(error);
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: AuthForm(_submitAuthForm, _isLoading));
+  }
+}
